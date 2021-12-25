0f88cc6b2da31404c32a0c286c1c18ac9530fae3
James S. Wang <JJamesWWang@gmail.com>
2021-12-25 17:41:39 -0500

    Add css to center lists properly

--- src/widgets/kvocab/components/Analysis.tsx
+++ src/widgets/kvocab/components/Analysis.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import { AnalysisData } from "../KVocabApp";
+import classes from "./Analysis.module.css";
 
 const Analysis: FC<AnalysisData> = (props) => {
   const generateWordList = (wordsCategory: string, words: string[]) => {
     return (
-      <div>
+      <div className={classes.Analysis}>
         <h3>{`${wordsCategory}: ${words.length}`}</h3>
         <ol>
           {words.map((word) => (
@@ -19,8 +20,7 @@ const Analysis: FC<AnalysisData> = (props) => {
     <>
       {props.exact && generateWordList("Words matched exactly", props.exact)}
       {props.almost && generateWordList("Words almost matched", props.almost)}
-      {props.unmatched &&
-        generateWordList("Words not matched", props.unmatched)}
+      {props.unmatched && generateWordList("Words not matched", props.unmatched)}
     </>
   );
 };
