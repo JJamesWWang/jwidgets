9609e2abf0889c1a6029985471e59b2153eb9aed
James S. Wang <JJamesWWang@gmail.com>
2021-12-25 13:38:08 -0500

    Add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from "./App";
+import KVocabApp from "./widgets/kvocab/KVocabApp";
+
+render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/kvocab" element={<KVocabApp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/widgets/kvocab/KVocabApp.tsx
@@ -0,0 +1,5 @@
+function KVocabApp() {
+  return <h2>KVocab</h2>;
+}
+
+export default KVocabApp;
